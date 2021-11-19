bb95b24de90ec0e8cfe5398aa572a47986404917
SDB14 <cwruboot22@gmail.com>
2021-11-19 17:49:56 -0500

    index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { SHOWTABLES } = require('sequelize/types/lib/query-types');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:"category_id"
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:"category_id"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through:ProductTag,
+  foreignKey:"product_id"
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through:ProductTag,
+  foreignKey:"tag_id"
+})
 
 module.exports = {
   Product,
@@ -18,3 +33,13 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// Category
+// shoes
+// id (primary key) - 33
+
+
+// Product
+// Jordan's
+// id (primary key)
+// foreign key (referencing primary key on Category) 33
